7b6a058fbeb01fb6071d76a5c8510bb4ac5fd227
zb_global <zb_global@qq.com>
2020-03-29 21:02:45 +0800

    添加修改

--- src/main/java/com/springboot/ceshi/CeshiApplication.java
+++ src/main/java/com/springboot/ceshi/CeshiApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CeshiApplication {
 
+	/**
+	 * zhulei
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(CeshiApplication.class, args);
 	}
