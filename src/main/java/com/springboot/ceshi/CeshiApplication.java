2dabf9240208e8815a1eb578d64a244364a048e4
zb_global <zb_global@qq.com>
2020-03-29 21:42:40 +0800

    tiani

--- src/main/java/com/springboot/ceshi/CeshiApplication.java
+++ src/main/java/com/springboot/ceshi/CeshiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CeshiApplication {
 
 	/*
-	* 主类
+	* 主类qq
 	 * @param args
 	 */
 	public static void main(String[] args) {
