bd36eafbd1b74208110acf3b1c2ea09c9bec62cf
zb_global <zb_global@qq.com>
2020-03-29 21:04:29 +0800

    update main

--- src/main/java/com/springboot/ceshi/CeshiApplication.java
+++ src/main/java/com/springboot/ceshi/CeshiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CeshiApplication {
 
 	/**
-	 * zhulei
+	 * zhuleidsafdsfa
 	 * @param args
 	 */
 	public static void main(String[] args) {
